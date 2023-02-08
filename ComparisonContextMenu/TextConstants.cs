767218b7ee73345bd0e2c59bad3b7cc314c7c676
AzureAD\VanDangThai <van.dang@aswhiteglobal.com>
2023-02-08 22:25:00 +0700

    change text

--- ComparisonContextMenu/TextConstants.cs
+++ ComparisonContextMenu/TextConstants.cs
@@ -4,7 +4,7 @@ namespace ComparisonContextMenu
     {
         public const string SelectToCompare = "Select to compare";
         public const string Compare = "Compare";
-        public const string CompareWindow = "Compare window";
+        public const string CompareWindow = "Open compare window";
         public static string BadCompareToolConfig(string configPath) => $"Please check the compare tool path at path {configPath}";
         public static string CompareExecutionFail(string configPath) => $"Something went wrong. Check out the log file at path {configPath}";
     }
